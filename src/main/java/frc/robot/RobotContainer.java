d99aaeda299ae046817999bd89bf70c7ab6866dc
djoshi82 <139362492+djoshi82@users.noreply.github.com>
2023-07-17 18:22:30 -0400

    PS4 controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,18 +3,25 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
 import edu.wpi.first.wpilibj2.command.Command;
-
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.*;
 import frc.robot.commands.*;
 
 public class RobotContainer {
+  private final PS4Controller controller = new PS4Controller(0);
+  private final JoystickButton r2 = new JoystickButton(controller, 7);
+  private final Drivebase drivesub = new Drivebase();
+
   public RobotContainer() {
     configureBindings();
+    drivesub.setDefaultCommand(new DriveToDistance(drivesub));
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    r2.onTrue(new DriveToDistance(drivesub));
+  }
 
   public Command getAutonomousCommand() {
     return null;
