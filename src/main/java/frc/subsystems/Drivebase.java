c81a1f3fe473fe0c3c7faf8a91246035a183b13a
djoshi82 <139362492+djoshi82@users.noreply.github.com>
2023-07-17 17:59:06 -0400

    Drivebase

--- src/main/java/frc/subsystems/Drivebase.java
+++ src/main/java/frc/subsystems/Drivebase.java
@@ -5,23 +5,10 @@
 package frc.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
 
 public class DriveBaseSubsystem extends SubsystemBase {
   /** Creates a new DriveBaseSubsystem. */
-
-  CANSparkMax motor1;
-  CANSparkMax motor2;
-  CANSparkMax motor3;
-  CANSparkMax motor4;
-
-  public DriveBaseSubsystem() {
-    motor1 = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
-    motor2 = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-    motor3 = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-    motor4 = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless); 
-  }
+  public DriveBaseSubsystem() {}
 
   @Override
   public void periodic() {
